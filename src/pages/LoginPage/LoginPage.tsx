45aab684bc1bdab61cff5e719c8bac3f64f6d9b7
aJarosh <aarosz20@gmail.com>
2024-07-13 12:19:33 +0200

    create login form in login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,22 @@
 // import React from 'react'
 import './LoginPage.css'
-import img from '../../assets/loginImage.jpg'
+//import img from '../../assets/loginImage.jpg'
+import LoginForm from '../../components/LoginForm/LoginForm'
+import ToggleTheme from '../../components/ToggleTheme/ToggleTheme'
 
 function LoginPage() {
   return (
     <div className='login-page-container'>
       <div className='login-page-img'>
-        <img src={img} alt="login image" />
+        {/* <img src={img} alt="login image" /> */}
       </div>
       <div className='login-page-form'>
-        
-      </div>
-    </div>
+        <div className='toggle-theme'>
+          <ToggleTheme />
+        </div>
+        <LoginForm />
+      </div >
+    </div >
   )
 }
 
