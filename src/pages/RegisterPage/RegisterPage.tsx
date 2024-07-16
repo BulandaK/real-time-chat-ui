b3451292919d0efead01a95756b99a042b2eb99c
Bulanda <kamilbulandus@gmail.com>
2024-07-16 10:56:44 +0200

    add custom TexdFiled to register form

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import { Button, TextField } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import "./RegisterPage.css";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -9,6 +10,31 @@ type Inputs = {
   email: string;
   password: string;
 };
+const CustomCssInput = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "#448FA3",
+  },
+  "& label": { color: "#448FA3" },
+  "& .MuiFilledInput-underline:after": {
+    borderBottomColor: "#68C5DB",
+  },
+  "& .MuiFilledInput-underline:before": {
+    borderBottomColor: "#68C5DB",
+  },
+  "& .MuiFilledInput-root": {
+    // backgroundColor: "#448FA3", // Background color for filled variant
+    "&:hover": {
+      backgroundColor: "#68C5DB",
+    },
+    "&.Mui-focused": {
+      backgroundColor: "#68C5DB",
+      color: "#fff",
+    },
+  },
+  "& .MuiInputBase-input": {
+    // color: "#fff", // Text color for better contrast with the background
+  },
+});
 
 const RegisterPage = () => {
   const {
@@ -28,27 +54,32 @@ const RegisterPage = () => {
   console.log(errors);
   return (
     <div className="reg-container">
-      <h2 className="reg-title">Create an account</h2>
+      <h2 className="reg-title">Create an account to Real Time Chat</h2>
 
       <form onSubmit={handleSubmit(onSubmit)} className="reg-form">
         {/* register your input into the hook by invoking the "register" function */}
 
-        <TextField
+        <CustomCssInput
           label="name"
           {...register("name", { required: true })}
-          variant="outlined"
+          variant="filled"
           error={errors.name ? true : false}
         />
-        <TextField
+        <CustomCssInput
           label="surname"
           {...register("surname", { required: true })}
-          variant="outlined"
+          variant="filled"
         />
-        <TextField label="email" {...register("email", { required: true })} />
+        <CustomCssInput
+          label="email"
+          variant="filled"
+          {...register("email", { required: true })}
+        />
 
-        <TextField
+        <CustomCssInput
           label="password"
           type="password"
+          variant="filled"
           {...register("password", {
             required: true,
             minLength: { value: 8, message: "min length is 8" },
