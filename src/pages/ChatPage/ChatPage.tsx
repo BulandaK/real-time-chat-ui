c87a645fc2560fa0338039096e7c14d41b7c12d6
BulandaK <eskimosl@wp.pl>
2024-07-19 12:24:45 +0200

    create side bar in ChatPage

--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -1,7 +1,39 @@
 // import React from "react";
 
+import "./ChatPage.css";
+import UserInfo from "./UserInfo/UserInfo";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import GroupIcon from "@mui/icons-material/Group";
+import DialpadIcon from "@mui/icons-material/Dialpad";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { Button } from "@mui/material";
+import CustomCssInput from "../../components/CustomCssInput/CustomCssInput";
+import ChatItem from "./ChatItem/ChatItem";
+
 function ChatPage() {
-  return <div>ChatPage</div>;
+  return (
+    <div className="chat-page-container">
+      <div className="side-bar">
+        <UserInfo />
+        <div className="icons">
+          <Button size="large" startIcon={<ChatBubbleIcon />} />
+          <Button size="large" startIcon={<DialpadIcon />} />
+          <Button size="large" startIcon={<GroupIcon />} />
+          <Button size="large" startIcon={<SettingsIcon />} />
+        </div>
+        <div className="search-box">
+          <CustomCssInput label="search-chats" />
+        </div>
+        <div className="user-chats">
+          <ChatItem />
+          <ChatItem />
+          <ChatItem />
+          <ChatItem />
+        </div>
+      </div>
+      <div className="chat-content"></div>
+    </div>
+  );
 }
 
 export default ChatPage;
